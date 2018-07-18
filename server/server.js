af0d73deb5cdfc72ab18ff1856ead90b82a319e2
Nathan Carroll <ntc238@gmail.com>
2018-07-18 16:26:39 -0500

    set up file structure and npm

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
