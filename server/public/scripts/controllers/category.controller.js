91c937fb46dad75c6c95cbbba0d3e3bdf29376cf
Nathan Carroll <ntc238@gmail.com>
2018-07-22 17:27:18 -0500

    mdDialog to stop you from deleting a category with books in it

--- server/public/scripts/controllers/category.controller.js
+++ server/public/scripts/controllers/category.controller.js
@@ -11,6 +11,37 @@ app.controller('CategoryController', ['BookTrackerService', '$mdDialog', '$mdToa
     self.books = BookTrackerService.books;
     self.categories = BookTrackerService.categories;
 
+    self.confirmDelete = function(category){
+        if (category.count > 0){
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .title('Error')
+                    .textContent('You can\'t delete a category with books in it.')
+                    .ariaLabel('Delete Category Alert')
+                    .ok('OK')
+            )
+        } else {
+            const confirm = $mdDialog.confirm()
+                .title('Are you sure you want to delete this category?')
+                .ariaLabel('Delete category confirmation')
+                .cancel('Cancel')
+                .ok('Delete');
+
+                $mdDialog.show(confirm).then(function(){
+                    console.log('delete case');
+                    BookTrackerService.deleteCategory(category);
+                    $mdToast.show(
+                        $mdToast.simple()
+                        .textContent(category.category + ' has been deleted.')
+                        .position('top right')
+                        .hideDelay(3000)
+                    );
+                }, function(){
+                    console.log('cancel case');
+                })
+            }
+    }
+
     BookTrackerService.getCategories();
     BookTrackerService.getBooks();
 }]);
