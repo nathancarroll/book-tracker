e916a375c095d0cb05c3a22543d8448ce13a6a99
Nathan Carroll <ntc238@gmail.com>
2018-07-23 09:11:04 -0500

    update database with more test data

--- database.sql
+++ database.sql
@@ -9,17 +9,26 @@ CREATE TABLE "books" (
 	"title" VARCHAR(120) NOT NULL,
 	"author" VARCHAR(40) NOT NULL,
 	"category_id" INT REFERENCES "categories",
-	"image_path" VARCHAR(120),
-	"pages" INTEGER,
+	"image_path" VARCHAR(255),
 	"read" BOOLEAN DEFAULT false,
 	"date_completed" DATE DEFAULT NULL
 );
 
 -- Here are some initial categories you can use to get your database started
 INSERT INTO "categories" ("category") VALUES 
-('History/Non-Fiction'),
-('Science/Non-Fiction'),
-('Sports/Non-Fiction'),
-('Mystery/Fiction'),
-('Sci-Fi/Fiction'),
-('Drama/Fiction');
+('History'),
+('Science'),
+('Sports'),
+('Mystery'),
+('Sci-Fi'),
+('Drama'),
+('Biography')
+
+-- Here are some initial books you can insert for testing.
+-- Note that these are meant to work only with the categories listed above.
+INSERT INTO "books" ("title", "author", "category_id", "image_path") VALUES 
+('Open', 'Andre Agassi', 3, '../assets/open.jpg'),
+('The Godfather', 'Mario Puzo', 6, '../assets/the-godfather.jpg'),
+('Master of the Senate', 'Robert Caro', 7, '../assets/master-of-the-senate.jpg'),
+('Lonesome Dove', 'Larry McMurty', 6, '../assets/lonesome-dove.jpg'),
+('Anathem', 'Neal Stephenson', 5, '../assets/anathem.jpg');
